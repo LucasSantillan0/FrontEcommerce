51396737bc77a3e7f4de64068178619242abd9e3
jomaMP <mpjm.acc@gmail.com>
2022-01-18 22:44:55 -0300

    generando datos para categories

---
+++ src/components/CategoriesFilter/CategoriesFilter.js
@@ -0,0 +1,30 @@
+import react from "react"
+
+const CategoriesFilter = () => {
+    return (
+        <div class="container">
+            <div class="row align-items-start">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="electrodomesticos" aria-label="..." />
+                        electrodomesticos
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="computacion" aria-label="..." />
+                        computación
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="indumentaria" aria-label="..." />
+                        indumentaria
+                    </li>
+                    <li class="list-group-item">
+                        <input class="form-check-input me-1" type="checkbox" value="indumentaria" aria-label="..." />
+                        vehicular
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default CategoriesFilter
--- src/containers/Categories/Categories.js
+++ src/containers/Categories/Categories.js
@@ -1,9 +1,31 @@
-import react from 'react'
+
+import CategoriesFilter from '../../components/CategoriesFilter/CategoriesFilter'
+import Product from '../../components/Product/Product'
+
+import faker from "faker"
+let acc = []
+let i = 0
+while(i <= 12) {
+    acc = [...acc, {
+        name:faker.commerce.productName(),
+        price:faker.commerce.price(),
+        image:faker.image.people(),     
+    }]
+    i++
+}
 
 const Categories = () => {
     return (
-        <div>
-            <h1>CATEGORIAS</h1>
+        <div className="container-fluid">
+            <div className="row border">
+                <div className="col-3">
+                    <CategoriesFilter />
+                </div>
+                <div className="col-4">
+                    {acc.map(e => <Product name={e.name} price={e.price} image={e.image}/>)} 
+                </div>
+            </div>
+            
         </div>
     )
 }
